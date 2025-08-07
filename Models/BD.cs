3634e849b82e42f3d4f19f07be84656f36c97b3a
FELIPE BECKFORD SISIS <49255481@ort>
2025-08-07 14:08:00 -0300

    Primer Commit

---
+++ Models/BD.cs
@@ -0,0 +1,12 @@
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace TP06_ToDoList;
+
+static public class BD
+{
+    private static string _connectionString = @"Server=localhost;DataBase=Presentacion;Integrated Security=True;TrustServerCertificate=True;";
+
+    
+
+}
