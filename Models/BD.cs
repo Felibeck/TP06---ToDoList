f250635c934d1d4a17fd53ec38582972a729f66f
FELIPE BECKFORD SISIS <49255481@ort>
2025-08-07 14:30:27 -0300

    Model tarea y usuario terminados y BD empezado

--- Models/BD.cs
+++ Models/BD.cs
@@ -7,6 +7,31 @@ static public class BD
 {
     private static string _connectionString = @"Server=localhost;DataBase=Presentacion;Integrated Security=True;TrustServerCertificate=True;";
 
-    
+
+
+// login
+     public static int Login(string username, string password)
+    {
+        int IDusuarioBuscado = -1;
+        using(SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT ID FROM Usuarios WHERE password = @pPassword";
+            IDusuarioBuscado = connection.QueryFirstOrDefault<int>(query, new { pPassword = password});
+        }
+            return IDusuarioBuscado;
+    }
+
+// sign in
+    public static int SignIn(string username, string password, string nombre, string apellido, string foto, string fechaUltimoLogin)
+    {
+        if(Login(username, password) == -1)
+        {
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Usuario VALUES ()"
+            }
+
+        }
+    }
 
 }
---
+++ Models/Tarea.cs
@@ -0,0 +1,9 @@
+public class Tarea
+{
+    public int ID {get; private set;}
+    public string titulo {get; private set;}
+    public string descripcion {get; private set;}
+    public DateTime fecha {get; private set;}
+    public bool finalizada {get; private set;}
+    public int IDUsuario {get; private set;}
+}
